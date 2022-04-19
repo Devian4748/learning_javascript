ecd9d3a532481c49b529d6c06ee4f2eca4b4c4d1
Devian4748 <devian4748@gmail.com>
2022-04-20 07:36:19 +0900

    Add Analysis name functions

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-import { getNumberOfChars } from './variations.js';
+import {
+  getCapitalized,
+  getFirstChar,
+  getLastChar,
+  getLower,
+  getNumberOfChars,
+  getUpper,
+} from './variations.js';
 
 const answer1 = document.querySelector('#answer1');
+const answer2 = document.querySelector('#answer2');
+const answer3 = document.querySelector('#answer3');
+const answer4 = document.querySelector('#answer4');
+const answer5 = document.querySelector('#answer5');
+const answer6 = document.querySelector('#answer6');
 
 const name = document.querySelector('#your-name');
 
@@ -8,4 +20,13 @@ name.addEventListener('keyup', () => {
   const value = name.value;
 
   answer1.textContent = getNumberOfChars(value);
+  answer2.textContent = getFirstChar(value);
+  answer3.textContent = getLastChar(value);
+  answer4.textContent = getLower(value);
+  answer5.textContent = getUpper(value);
+  if (value) {
+    answer6.textContent = getCapitalized(value);
+  } else {
+    answer6.textContent = '';
+  }
 });
--- variations.js
+++ variations.js
@@ -4,3 +4,38 @@ export function getNumberOfChars(name) {
 
   return name.length;
 }
+
+/**@param {string} name */
+export function getFirstChar(name) {
+  // first character of name : name
+
+  return name[0];
+}
+
+/**@param {string} name */
+export function getLastChar(name) {
+  // last character of : name
+  return name[name.length - 1];
+}
+
+/**@param {string} name */
+export function getLower(name) {
+  // name all in lower case ( example : 'ABC' becomes 'abc' )
+
+  return name.toLowerCase();
+}
+
+/**@param {string} name */
+export function getUpper(name) {
+  // name all in upper case ( example : 'abc' becomes 'ABC' )
+  return name.toUpperCase();
+}
+
+/**@param {string} name */
+export function getCapitalized(name) {
+  // capitalized version of name ( example : 'alEX' becomes 'Alex' )
+
+  const firstChar = name[0].toUpperCase();
+  const remainStr = name.substring(1).toLowerCase();
+  return firstChar + remainStr;
+}
