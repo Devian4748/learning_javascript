6c620f8e1eea7f85ae6fa5e39abc0b709fd5b60f
Devian4748 <devian4748@gmail.com>
2022-04-23 13:12:19 +0900

    add get division remainder

--- measure.js
+++
@@ -1,7 +0,0 @@
-/**
- * @param {string} value
- */
-
-export function getBoxWidth(value) {
-  return Number.parseInt(value, 10);
-}
---
+++ numbers.js
@@ -0,0 +1,7 @@
+/**
+ * @param {string} number
+ */
+
+export function getDivisionRemainderBy2(number) {
+  return Number.parseInt(number, 10) % 2;
+}
