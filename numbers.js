48e7b12cf685a4b99498ebc3e9a727a0836449af
Devian4748 <devian4748@gmail.com>
2022-04-23 13:47:56 +0900

    instead return 0 or 1, return even or odd

--- numbers.js
+++ numbers.js
@@ -1,7 +1,15 @@
 /**
  * @param {string} number
  */
-
 export function getDivisionRemainderBy2(number) {
   return Number.parseInt(number, 10) % 2;
+}
+
+/**
+ * @param {string} number
+ */
+export function evenOrOdd(number) {
+  const absNum = Math.abs(Number.parseInt(number, 10));
+
+  return absNum % 2 === 0 ? 'even' : 'odd';
 }
