b58368c455bee85693d9c31ee8ed6590aeb57f3c
Devian4748 <devian4748@gmail.com>
2022-04-23 13:03:21 +0900

    add get box width

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import { getNextAge } from './age.js';
+import { getBoxWidth } from './measure.js';
 
-const age = document.querySelector('#your-age');
-const nextAge = document.querySelector('#next-age');
+const box1 = document.querySelector('#box1');
+const box2 = document.querySelector('#box2');
 
-age.addEventListener('keyup', () => {
-  nextAge.textContent = getNextAge(age.value);
-});
+const box1Value = getComputedStyle(box1).width;
+const box2Value = getComputedStyle(box2).width;
+
+console.log('First box width', getBoxWidth(box1Value));
+console.log('Second box width', getBoxWidth(box2Value));
