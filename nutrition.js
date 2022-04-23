ab34fd20c710d9fd90b8ccf5549fd9da4e99f233
Devian4748 <devian4748@gmail.com>
2022-04-23 20:46:46 +0900

    문자열로 각각 넣던 Options 를 Array 를 통해서 넣는 방식으로 수정

--- nutrition.js
+++ nutrition.js
@@ -7,3 +7,21 @@ export function renderTableRow(label, value) {
 
   return `<tr><td>${label}</td><td>${value}</td></tr>`;
 }
+
+/**
+ * @param {array[][]} rows
+ */
+export function renderTableRows(rows) {
+  console.log(rows);
+
+  let tableRows = '';
+
+  rows.forEach(function (row) {
+    tableRows += `<tr>
+      <td>${row[0]}</td>
+      <td>${row[1]}</td>
+    </tr>`;
+  });
+
+  return tableRows;
+}
