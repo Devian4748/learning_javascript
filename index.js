ea5af4e139614378d7d4757fb9426e36d6a0bbd9
Devian4748 <devian4748@gmail.com>
2022-04-23 22:34:16 +0900

    Add classroom

--- index.js
+++ index.js
@@ -1,12 +1,56 @@
-import { renderTableRows } from './nutrition.js';
+import {
+  getAverageValue,
+  getFailingGrades,
+  getNumberOfGrades,
+  getPassingGrades,
+  getRaisedGrades,
+  getSumGrades,
+} from './classroom.js';
 
-const data = [
-  ['Carbs', '17g'],
-  ['Protein', '19g'],
-  ['Fat', '5g'],
-];
+const gradesForm = document.querySelector('#grades-form');
+const yourGrade = document.querySelector('#your-grade');
 
-const html = renderTableRows(data);
+const grades = [12, 19, 10, 4, 15, 9];
 
-const tbody = document.querySelector('#nutrition-table tbody');
-tbody.insertAdjacentHTML('beforeend', html);
+/**
+ * @param {number[]} grades
+ */
+const renderStats2Table = grades => {
+  const tbody = document.querySelector('#stats-table2 tbody');
+  tbody.innerHTML = `<tr>
+    <td>${getPassingGrades(grades)}</td>
+    <td>${getFailingGrades(grades)}</td>
+    <td>${getRaisedGrades(grades)}</td>
+  </tr>`;
+};
+
+/**
+ * @param {number[]} grades
+ */
+const renderStatsTable = grades => {
+  const tbody = document.querySelector('#stats-table tbody');
+  tbody.innerHTML = `<tr>
+    <td>${getNumberOfGrades(grades)}</td>
+    <td>${getSumGrades(grades)}</td>
+    <td>${getAverageValue(grades)}</td>
+  </tr>`;
+};
+
+/**
+ * @param {number[]} grades
+ */
+const render = grades => {
+  console.log('Grades: ' + grades.join(', '));
+  renderStatsTable(grades);
+  renderStats2Table(grades);
+};
+
+gradesForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const newGrade = Number.parseInt(yourGrade.value, 10);
+  grades.push(newGrade);
+  yourGrade.value = ``;
+  render(grades);
+});
+
+render(grades);
