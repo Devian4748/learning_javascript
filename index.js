b269a4370989fa53286f5543fc59474913109d74
Devian4748 <devian4748@gmail.com>
2022-04-23 13:33:31 +0900

    add get dicision remainder

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import { getBoxWidth } from './measure.js';
+import { getDivisionRemainderBy2 } from './numbers.js';
 
-const box1 = document.querySelector('#box1');
-const box2 = document.querySelector('#box2');
+const number = document.querySelector('#your-number');
+const output = document.querySelector('#output');
 
-const box1Value = getComputedStyle(box1).width;
-const box2Value = getComputedStyle(box2).width;
-
-console.log('First box width', getBoxWidth(box1Value));
-console.log('Second box width', getBoxWidth(box2Value));
+number.addEventListener('keyup', () => {
+  output.textContent = getDivisionRemainderBy2(
+    Number.parseInt(number.value, 10)
+  );
+});
